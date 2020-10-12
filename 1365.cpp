fc514baca38cee9a9ec18a0476a6aa376766e21c
garlicisnotmyfavor <garlicisnotmyfavor@outlook.com>
2020-10-12 18:28:35 +0800

    2020-10-12

---
+++ 1365.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> cnt(101, 0);
+        vector<int> vec((int)nums.size(), 0);
+        for (int i = 0;i < (int)nums.size(); ++i){
+            cnt[nums[i]]++;
+        }   
+        for (int i = 1;i <= 100; ++i) cnt[i] += cnt[i-1]; // 求前缀和
+        for (int i = 0;i < (int)nums.size(); ++i){
+            if (nums[i]) vec[i] = cnt[nums[i] - 1];
+        } 
+        return vec;
+    }
+};
